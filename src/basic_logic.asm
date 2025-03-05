b4a09beef351eb2723371f4f525a0271e887991f
Manav Maheshwari <manavm712@gmail.com>
2025-03-05 15:52:12 -0400

    Update basic_logic.asm

--- src/basic_logic.asm
+++ src/basic_logic.asm
@@ -1,5 +1,6 @@
 .data 
-floorRequest: .asciiz "Enter the desired floor you want to reach to: "
+targetFloor: .asciiz "Enter the desired floor you want to reach to: "
+currentFloor: .asciiz "Enter the floor you're currently on: "
 movingUp: .asciiz "You are moving up to : "
 movingDown: .asciiz "You are moving down to : "
 floorReached: .asciiz "You have reached your desired floor "
@@ -7,41 +8,38 @@ floorReached: .asciiz "You have reached your desired floor "
 .text
 .globl main
 main:
-	li $v0, 4
-	la $a0, targetFloor
-	syscall
-	#Reading the floor
-	li $v0, 5
-	syscall
-	move $t0, $v0
+li $v0, 4
+la $a0, targetFloor
+syscall
+#Reading the floor
+li $v0, 5
+syscall
+move $t0, $v0
 
-	li $v0, 4
-	la $a0, currentFloor
-	syscall
-	#Reading the floor
-	li $v0, 5
-	syscall
-	move $t1, $v0
-	
-	#moving the elevator accordingly
-	blt $t0, $t1, moving_up
-	bgt $t0, $t1, moving_down
-
+li $v0, 4
+la $a0, currentFloor
+#Reading the floor
+li $v0, 5
+syscall
+move $t1, $v0
+#moving the elevator accordingly
+blt $t0, $t1, moving_up
+bgt $t0, $t1, moving_down
 moving_up:
-	addi $t0, $t0, 1 			# incrementing the floor
+	addi $t0, $t0, 1 #incrementing the floor
 	li $v0, 4
-	la $a0, movingUp 			# printing out the floor message
-	syscall 				# issuing a systemcall
+	la $a0, movingUp #printing out what floor you're going up
+	syscall #issuing a systemcall
 
-	j reached 				# printing out the reached floor 
+j reached #printing out the reached floor 
 
 moving_down:
-	addi $t0, $t0, -1
+	addi $t0, $t0, -1 #decrementing the floor 
 	li $v0, 4
-	la $a0, movingDown
+	la $a0, movingDown #printing out what floor you're going down
 	syscall
 
-	j reached
+j reached
 
 reached:
 	#dale's dequeue function.
